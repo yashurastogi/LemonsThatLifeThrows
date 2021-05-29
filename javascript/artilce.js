b75f544544c6b89c29534bc500c863303bba1048
yashurastogi <rastogi.yashu2010@gmail.com>
2021-05-29 11:36:32 +0530

    commit

--- javascript/artilce.js
+++ javascript/artilce.js
@@ -41,5 +41,28 @@ export let article = {
                     one file the whole day? The answer is distractions.`
 
     },
+    "article4": {
+        date: '29th May, 2021',
+        link: 'productive.html',
+        heading: ' 12 Ways to be more productive',
+        category: 'Simpler Life',
+        img: 'img/productive.jpg',
+        content: ` My colleague, to me one evening : “Hey, I have been trying to complete this file since morning and
+                    still I am only half
+                    done.”
+                    <br>
+                    Me: “Is it too big to finish in a day?”
+                    <br>
+                    Colleague: “That’s the thing, it is not too big. I could have easily finished it today”
+                    
+                    <br>
+                    --------------------------------------------------------------------------------------
+                    <br>
+                    Have you ever been in a situation as my colleague above? At least I have been. So why do you think
+                    people can’t finish
+                    one file the whole day? The answer is distractions.`
+
+    },
+
 
 }
